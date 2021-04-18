d6ec92dbd17d98c88c9774b81bb372f09d7ed50e
async-devil <async.devil@gmail.com>
2021-04-18 23:36:48 +0300

    feat: added wallet module

---
+++ src/Transaction/Wallet.ts
@@ -0,0 +1,117 @@
+import Balance from "./Balance";
+import PaymentMethod, { PaymentMethods } from "./Categories/PaymentMethod";
+import Transaction, { TransactionDetails, TransactionDetailsUnparsed } from "./Transaction";
+
+class Wallet {
+	private transactions: Transaction[] = [];
+	private balanceList: Balance[] = [];
+
+	/** Use undefined or empty array if need to skip */
+	constructor(customBalances: PaymentMethod[] = []) {
+		const balances = [
+			PaymentMethods["Cash"],
+			PaymentMethods["Card"],
+			PaymentMethods["Other"],
+			...customBalances,
+		];
+		balances.forEach((balance) => this.balanceList.push(new Balance(balance)));
+	}
+
+	private findRelativeBalance(transaction: Transaction): Balance {
+		const balance = this.balanceList.find(
+			(balance) => balance.type === transaction.details.paymentMethod.toString()
+		);
+
+		if (!balance) throw new Error("Balance not found");
+		return balance;
+	}
+
+	private doDependOnType(type: string, doOnIncome: () => any, doOnExpense: () => any) {
+		if (type === "Income") return doOnIncome();
+		if (type === "Expense") return doOnExpense();
+		throw new Error("Invalid type");
+	}
+
+	/** On income add, on expense subtract */
+	private normalBalanceOperation(balance: Balance, transaction: Transaction) {
+		this.doDependOnType(
+			transaction.details.type,
+			() => balance.add(transaction.details.amount),
+			() => balance.subtract(transaction.details.amount)
+		);
+	}
+
+	/** On income subtract, on expense add */
+	private reverseBalanceOperation(balance: Balance, transaction: Transaction) {
+		this.doDependOnType(
+			transaction.details.type,
+			() => balance.subtract(transaction.details.amount),
+			() => balance.add(transaction.details.amount)
+		);
+	}
+
+	deleteTransactionByID(id: string): Transaction {
+		const deleteTransactionIndex = this.transactions.findIndex(
+			(transaction) => transaction.details.id === id
+		);
+
+		if (deleteTransactionIndex === -1) throw new Error("Transaction not found");
+		const deleteTransaction = this.transactions[deleteTransactionIndex];
+
+		this.transactions.splice(deleteTransactionIndex, 1);
+
+		const balance = this.findRelativeBalance(deleteTransaction);
+		this.reverseBalanceOperation(balance, deleteTransaction);
+
+		return deleteTransaction;
+	}
+
+	createTransaction(
+		transactionDetails: TransactionDetails | TransactionDetailsUnparsed
+	): Transaction {
+		const transaction = new Transaction(transactionDetails);
+		const balance = this.findRelativeBalance(transaction);
+
+		this.normalBalanceOperation(balance, transaction);
+		this.transactions.push(transaction);
+
+		return transaction;
+	}
+
+	updateTransactionByID(
+		id: string,
+		updatedDetails: TransactionDetails | TransactionDetailsUnparsed
+	): Transaction {
+		const updateTransaction = this.transactions.find(
+			(transaction) => transaction.details.id === id
+		);
+		if (!updateTransaction) throw new Error("Transaction not found");
+
+		const balance = this.findRelativeBalance(updateTransaction);
+		this.reverseBalanceOperation(balance, updateTransaction);
+
+		try {
+			updateTransaction.setDetails = updatedDetails;
+		} catch (err) {
+			this.normalBalanceOperation(balance, updateTransaction);
+			throw err;
+		}
+
+		this.normalBalanceOperation(balance, updateTransaction);
+
+		return updateTransaction;
+	}
+
+	get transactionsList() {
+		return [...this.transactions];
+	}
+
+	getBalanceByType(type: string) {
+		const balance = this.balanceList.find((balance) => balance.type === type);
+		if (!balance) throw new Error("Balance not found");
+
+		return balance;
+	}
+}
+
+export default Wallet;
