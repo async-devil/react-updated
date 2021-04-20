45d480a2f8099836d7a772c32096af67985d68c0
async-devil <async.devil@gmail.com>
2021-04-20 23:41:44 +0300

    feat: created header

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+import { ThemeType } from "../../Theme/Theme";
+import Balance from "../../Transaction/Balance";
+
+const StyledHeader = styled.header<{ theme: ThemeType; isCurrent: boolean }>`
+	width: 100%;
+	height: ${(props) => props.theme.height.header};
+
+	right: 0px;
+	left: 0px;
+	top: 0px;
+
+	position: fixed;
+
+	background-color: #444444;
+	background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%234e4e4e' fill-opacity='0.53' fill-rule='evenodd'/%3E%3C/svg%3E");
+	${(props) =>
+		props.isCurrent &&
+		css`
+			background-color: #009989;
+			background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23128379' fill-opacity='0.34' fill-rule='evenodd'/%3E%3C/svg%3E");
+		`};
+`;
+
+const Header = ({ balance, isCurrent }: { balance: Balance; isCurrent: boolean }) => {
+	return (
+		<StyledHeader isCurrent={isCurrent}>
+			{balance.ammount} {balance.type}
+		</StyledHeader>
+	);
+};
+
+export default Header;
